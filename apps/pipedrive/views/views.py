77d21fb5ebe5025b2adbec205a2192dd6c6e9946
Anmol Goel <anmolgoel12@gmail.com>
2023-10-13 16:20:28 +0530

    description changed

--- apps/pipedrive/views/views.py
+++ apps/pipedrive/views/views.py
@@ -61,7 +61,7 @@ class LeadViewSet(ModelViewSet):
             "is_created": {"type": "Entity Created", "description": "Lead Created"},
         },
         "update": {
-            "status": {"type": "Status Update", "description": "Lead status changed"},
+            "status": {"type": "Status Update", "description": "Lead Status Updated"},
             "is_converted_to_prospect": {
                 "type": "State Update",
                 "description": "Lead Converted to Prospect",
@@ -237,7 +237,7 @@ class ProspectViewSet(ModelViewSet):
         "mapping_obj": "lead_id",
         "is_mapping_obj_func": False,
         "update": {
-            "status": {"type": "Status Update", "description": "Prospect status changed"},
+            "status": {"type": "Status Update", "description": "Prospect Status Updated"},
             "is_converted_to_deal": {
                 "type": "State Update",
                 "description": "Prospect Converted to Deal",
