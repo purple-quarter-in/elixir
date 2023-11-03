be8f4909a8ca3780896843811190206f67e811a6
Anmol Goel <anmolgoel12@gmail.com>
2023-11-03 11:11:01 +0530

    prod setup

---
+++ elixir/settings/production.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "elixir.purplequarter.co"]
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOWED_ORIGINS = ["https://api.elixir.purplequarter.co", "https://elixir.purplequarter.co"]
+CORS_ALLOW_HEADERS = [
+    "access-control-allow-origin",
+    "content-type",
+    "authorization",
+    "accept",
+]
+CSRF_TRUSTED_ORIGINS = ["https://api.elixir.purplequarter.co"]
+SITE_URL = "https://elixir.purplequarter.co"
