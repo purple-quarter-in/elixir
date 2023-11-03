2094aca2f98d5d330c8b2c2fea126a4d544ae8ec
Anmol Goel <anmolgoel12@gmail.com>
2023-11-04 00:13:56 +0530

    contact split flix

--- apps/client/views.py
+++ apps/client/views.py
@@ -138,7 +138,7 @@ class ContactViewSet(ModelViewSet):
         email = request.query_params.get("email", None)
         errors = {}
         if phone:
-            std, number = phone.split(" ")
+            std, number = phone.split("-")
             is_phone = Contact.objects.filter(phone=number, std_code=std).exists()
             is_duplicate = is_duplicate or is_phone
             errors["phone"] = is_duplicate
